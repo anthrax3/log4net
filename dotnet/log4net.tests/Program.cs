dbdce9dcd01dc6dce47ae15f1db3aaa09879fa60
Charles Lowell <chlowe@microsoft.com>
2016-02-12 13:23:04 -0800

    xUnit -> NUnitLite

---
+++ dotnet/log4net.tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using NUnitLite;
+using System;
+using System.Reflection;
+
+namespace log4net.tests
+{
+    public class Program
+    {
+        public int Main(string[] args)
+        {
+            return new AutoRun().Execute(typeof(Program).GetTypeInfo().Assembly, Console.Out, Console.In, args);
+        }
+    }
+}
