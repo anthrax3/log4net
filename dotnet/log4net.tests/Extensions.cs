1559114f42ca19662c0b1c6325b965acf2503d87
Charles Lowell <chlowe@microsoft.com>
2016-02-12 15:41:56 -0800

    Add extension methods to log4net.tests

---
+++ dotnet/log4net.tests/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace log4net.Tests
+{
+    internal static class Extensions
+    {
+        public static void Close(this FileStream fileStream) => fileStream.Dispose();
+        public static void Close(this StreamReader streamReader) => streamReader.Dispose();
+        public static ConstructorInfo GetConstructor(this Type type, BindingFlags bindingAttr, object binder, Type[] types, object[] modifiers) => type.GetConstructor(bindingAttr, binder, types, modifiers);
+        public static MethodInfo GetMethod(this Type type, string name, BindingFlags bindingAttr, object binder, Type[] types, object[] modifiers) => type.GetMethod(name, types);
+    }
+}
