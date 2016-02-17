766cb54c2aa6b800c4a67b8fd44f5945d64519d6
Charles Lowell <chlowe@microsoft.com>
2016-02-17 11:10:00 -0800

    Extensions.cs: expand syntax

--- dotnet/log4net.tests/Extensions.cs
+++ dotnet/log4net.tests/Extensions.cs
@@ -7,12 +7,17 @@ namespace log4net.Tests
     internal static class Extensions
     {
         public static void Close(this FileStream fileStream) => fileStream.Dispose();
+
         public static void Close(this StreamReader streamReader) => streamReader.Dispose();
+
         public static ConstructorInfo GetConstructor(this Type type, BindingFlags bindingAttr, object binder, Type[] types, object[] modifiers)
         {
             return type.GetConstructor(types);
         }
 
-        public static MethodInfo GetMethod(this Type type, string name, BindingFlags bindingAttr, object binder, Type[] types, object[] modifiers) => type.GetMethod(name, types);
+        public static MethodInfo GetMethod(this Type type, string name, BindingFlags bindingAttr, object binder, Type[] types, object[] modifiers)
+        {
+            return type.GetMethod(name, types);
+        }
     }
 }
